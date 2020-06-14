603bcd17c53b3e30c95020aaeaf7c7940c0c382e
AnaisUrlichs <33576047+AnaisUrlichs@users.noreply.github.com>
2020-06-14 17:02:04 +0200

    Create GameBehavior.cs

---
+++ GameBehavior.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CustomExtensions;
+
+public class GameBehavior : MonoBehaviour, IManager
+{
+    public string labelText = "Collect all 4 items and win your freedom!";
+    public readonly int maxItems = 4;
+    public bool showWinScreen = false;
+    public bool showLossScreen = false;
+
+    public delegate void DebugDelegate(string newText);
+    public DebugDelegate debug = Print;
+
+    private string _state;
+    public string State 
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    private int _itemsCollected = 0;
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set { 
+            _itemsCollected = value;
+
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You've found all the items!";
+                showWinScreen = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
+        }
+    }
+
+    private int _playerLives = 3;
+    public int Lives 
+    {
+        get { return _playerLives; }
+        set { 
+            _playerLives = value; 
+
+            if(_playerLives <= 0)
+            {
+                labelText = "You want another life with that?";
+                showLossScreen = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                labelText = "Ouch... that's got hurt.";
+            }
+        }
+    }
+
+    void Start()
+    {
+        Initialize();
+
+        InventoryList<string> inventoryList = new InventoryList<string>();
+        inventoryList.SetItem("Potion");
+        Debug.Log(inventoryList.item);
+    }
+
+    public void Initialize() 
+    {
+        _state = "Manager initialized..";
+        _state.FancyDebug();
+
+        debug(_state);
+        LogWithDelegate(debug);
+
+        PlayerBehavior playerBehavior = GameObject.Find("Player").GetComponent<PlayerBehavior>();
+        playerBehavior.playerJump += HandlePlayerJump;
+    }
+
+    public void HandlePlayerJump(bool isGrounded)
+    {
+        if(isGrounded)
+        {
+            debug("Player has jumped...");
+        }
+    }
+
+    public static void Print(string newText)
+    {
+        Debug.Log(newText);
+    }
+
+    public void LogWithDelegate(DebugDelegate debug)
+    {
+        debug("Delegating the debug task...");
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health: " + _playerLives);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 200, 100), "YOU WON!"))
+            {
+                Utilities.RestartLevel();
+            }
+        }
+
+        if(showLossScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You lose..."))
+            {
+                try
+                {
+                    Utilities.RestartLevel(-1);
+                    debug("Level restarted successfully...");
+                }
+                catch (System.ArgumentException e)
+                {
+                    Utilities.RestartLevel(0);
+                    debug("Reverting to scene 0: " + e.ToString());
+                }
+                finally
+                {
+                    Utilities.RestartLevel(0);
+                    debug("Restart handled...");
+                }
+            }
+        }
+    }
+}
